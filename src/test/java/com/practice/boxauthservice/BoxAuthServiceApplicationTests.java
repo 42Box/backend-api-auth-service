df881cdedd02bac7712ceb74016b08fc6f83cdb5
middlefitting <wjdtmdcjf2@gmail.com>
2023-08-20 01:18:06 +0900

    feat(root): 환경 설정

---
+++ src/test/java/com/practice/boxauthservice/BoxAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.boxauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxAuthServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
