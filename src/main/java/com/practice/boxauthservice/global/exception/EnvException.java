4c4b111b4156171c2a320d779c978bcd69b814bf
middlefitting <wjdtmdcjf2@gmail.com>
2023-08-21 00:47:26 +0900

    feat(root): 42Auth

---
+++ src/main/java/com/practice/boxauthservice/global/exception/EnvException.java
@@ -0,0 +1,9 @@
+package com.practice.boxauthservice.global.exception;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class EnvException extends RuntimeException {
+
+}
+
