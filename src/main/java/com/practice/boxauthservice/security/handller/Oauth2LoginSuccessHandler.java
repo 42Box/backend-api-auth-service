ea8c8f4992c3a1b8b617d443ccfa3ae02df47ca8
middlefitting <wjdtmdcjf2@gmail.com>
2023-08-25 03:51:00 +0900

    feat(root): 회원가입, 로그인 서비스 요청 구현

--- src/main/java/com/practice/boxauthservice/security/handller/Oauth2LoginSuccessHandler.java
+++ src/main/java/com/practice/boxauthservice/security/handller/Oauth2LoginSuccessHandler.java
@@ -1,6 +1,5 @@
 package com.practice.boxauthservice.security.handller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.practice.boxauthservice.global.env.EnvUtil;
 import com.practice.boxauthservice.global.jwt.JwtUtil;
 import java.io.IOException;
@@ -11,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
@@ -27,28 +27,29 @@ public class Oauth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
   public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
       Authentication authentication) throws IOException, ServletException {
     try {
-      OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
-      Map<String, Object> userInfo = parseUserInfo(oAuth2User);
-      Long userId = Long.valueOf(userInfo.get("userId").toString());
-      String jwtToken = jwtUtil.generateAccessJwtToken(userId);
+      String nickname = getNickname(authentication);
+      String jwtToken = jwtUtil.generateAccessJwtToken(nickname);
       Cookie jwtCookie = generateJwtCookie(jwtToken);
-      parseResponse(response, jwtCookie, jwtToken, userInfo);
+      parseResponse(response, jwtCookie);
     } catch (Exception e) {
       throw new OAuth2AuthenticationException("Authentication failed");
     }
   }
 
-  private Map<String, Object> parseUserInfo(OAuth2User oAuth2User) {
-    Object rawUserInfo = oAuth2User.getAttributes().get("user-info");
-    if (rawUserInfo instanceof Map) {
-      return (Map<String, Object>) rawUserInfo;
+  private String getNickname(Authentication authentication) {
+    String clientRegistration = ((OAuth2AuthenticationToken) authentication)
+        .getAuthorizedClientRegistrationId();
+    OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+    Map<String, Object> userInfo = oAuth2User.getAttributes();
+    if (clientRegistration.equals("42api")) {
+      return String.valueOf(userInfo.get("login"));
     } else {
-      throw new RuntimeException();
+      throw new OAuth2AuthenticationException("ClientRegistration Wrong!");
     }
   }
 
   private Cookie generateJwtCookie(String jwtToken) {
-    Cookie jwtCookie = new Cookie("jwt", jwtToken);
+    Cookie jwtCookie = new Cookie("box-jwt", jwtToken);
     int cookieAge = Integer.parseInt(envUtil.getEnv("jwt.token.ACCESS_EXPIRATION_TIME")) / 1000;
     jwtCookie.setHttpOnly(true);
     jwtCookie.setMaxAge(cookieAge);
@@ -57,15 +58,12 @@ public class Oauth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
     return jwtCookie;
   }
 
-  private void parseResponse(HttpServletResponse response, Cookie jwtCookie, String jwtToken,
-      Map<String, Object> userInfo) throws IOException {
-    ObjectMapper objectMapper = new ObjectMapper();
-    String json = objectMapper.writeValueAsString(userInfo);
+  private void parseResponse(HttpServletResponse response, Cookie jwtCookie) throws IOException {
+    response.setStatus(302);
+    response.setHeader("Location", envUtil.getEnv("header.auth-redirect-location.value"));
     response.addCookie(jwtCookie);
-    response.setHeader("Authorization", "Bearer " + jwtToken);
     response.setContentType("application/json");
     response.setCharacterEncoding("UTF-8");
     response.resetBuffer();
-    response.getWriter().write(json);
   }
 }
--- src/main/java/com/practice/boxauthservice/security/service/Oauth42UserService.java
+++ src/main/java/com/practice/boxauthservice/security/service/Oauth42UserService.java
@@ -1,20 +1,31 @@
 package com.practice.boxauthservice.security.service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.UUID;
+import lombok.AllArgsConstructor;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
+@AllArgsConstructor
 public class Oauth42UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+
+  private final DiscoveryClient discoveryClient;
 
   @Override
   public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -22,33 +33,67 @@ public class Oauth42UserService implements OAuth2UserService<OAuth2UserRequest, 
       ClientRegistration clientRegistration = userRequest.getClientRegistration();
       OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService = new DefaultOAuth2UserService();
       OAuth2User oAuth2User = oAuth2UserService.loadUser(userRequest);
-      Map<String, Object> mutableAttributes = new HashMap<>(oAuth2User.getAttributes());
-      Map<String, Object> user_info = signUp(clientRegistration, oAuth2User).orElseThrow(
-          RuntimeException::new);
-      mutableAttributes.put("user-info", user_info);
-      return new DefaultOAuth2User(oAuth2User.getAuthorities(), mutableAttributes,
-          clientRegistration.getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName());
+      signUp(clientRegistration, oAuth2User);
+      return oAuth2User;
     } catch (Exception e) {
       throw new OAuth2AuthenticationException("Registration failed");
     }
   }
 
-  private Optional<Map<String, Object>> signUp(ClientRegistration clientRegistration,
+  private void signUp(ClientRegistration clientRegistration,
       OAuth2User oAuth2User) {
     String registrationId = clientRegistration.getRegistrationId();
     if (registrationId.equals("42api")) {
-      return dummy42Result(oAuth2User);
+      try {
+        signUp42User(oAuth2User, clientRegistration);
+      } catch (Exception e) {
+        throw new OAuth2AuthenticationException("Registration failed");
+      }
+      return;
     }
     throw new OAuth2AuthenticationException("not supported registrationId");
   }
 
-  private Optional<Map<String, Object>> dummy42Result(OAuth2User oAuth2User) {
-    // 개발중인 더미 서비스
-    // user-service 에 요청을 보내 회원가입을 진행 예정
-    Map<String, Object> attributes = new HashMap<>();
-    attributes.put("userId", 1);
-    attributes.put("userUuid", UUID.randomUUID());
-    attributes.put("userNickname", oAuth2User.getName());
-    return Optional.of(attributes);
+  private void signUp42User(OAuth2User oAuth2User, ClientRegistration clientRegistration) {
+    Map<String, Object> attributes = new HashMap<>(oAuth2User.getAttributes());
+//    ArrayList<Integer> cursus_ids = (ArrayList<Integer>) attributes.get("cursus_ids"); // 보류
+    ArrayList<Map<String, Object>> campus_users = (ArrayList<Map<String, Object>>) attributes.get(
+        "campus_users");
+    Map<String, Object> firstMap = campus_users.get(0);
+
+    String nickname = attributes.get("login").toString();
+    int campus_id = Integer.parseInt(String.valueOf(firstMap.get("campus_id")));
+    String role = "ROLE_AUTH_USER";
+    postUserWithRestTemplate(nickname, campus_id, role);
+  }
+
+  private void postUserWithRestTemplate(String nickname, int campus_id, String role) {
+    String userServiceUrl = getServiceUrl("user-service");
+
+    RestTemplate restTemplate = new RestTemplate();
+    String endpoint = userServiceUrl + "/users";
+    Map<String, Object> requestBody = new HashMap<>();
+    requestBody.put("nickname", nickname);
+    requestBody.put("campusId", campus_id);
+    requestBody.put("role", role);
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(requestBody, headers);
+
+    ResponseEntity<String> response = restTemplate.postForEntity(endpoint, requestEntity,
+        String.class);
+    if (response.getStatusCode() != HttpStatus.CREATED
+        && response.getStatusCode() != HttpStatus.CONFLICT) {
+      throw new OAuth2AuthenticationException("Registration failed");
+    }
+  }
+
+  private String getServiceUrl(String serviceName) {
+    List<ServiceInstance> instances = discoveryClient.getInstances(serviceName);
+    if (instances != null && !instances.isEmpty()) {
+      return instances.get(0).getUri().toString();
+    }
+    throw new OAuth2AuthenticationException("discoveryClient failed");
   }
 }
