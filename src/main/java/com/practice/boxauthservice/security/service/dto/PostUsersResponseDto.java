b09b0a9a8277ccc90a9d6b9089042b153dede29f
middlefitting <wjdtmdcjf2@gmail.com>
2023-08-26 03:04:15 +0900

    fix(jwtUtil): add uuid, role in jwt

---
+++ src/main/java/com/practice/boxauthservice/security/service/dto/PostUsersResponseDto.java
@@ -0,0 +1,21 @@
+package com.practice.boxauthservice.security.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * PostUsersResponseDto.
+ *
+ * @author : middlefitting
+ * @description :
+ * @since : 2023/08/25
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class PostUsersResponseDto {
+
+  String role;
+  String uuid;
+}
